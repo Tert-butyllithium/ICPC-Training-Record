3e652ef66257a215316d022833ce07db6888efa8
Tert-butyllithium <40877238+Tert-butyllithium@users.noreply.github.com>
2018-07-19 10:33:15 +0800

    Create 离散化.cpp

---
+++ template/离散化.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#define maxn 100005
+using namespace std;
+
+int n;
+int a[maxn];
+vector <int> v;
+int getid(int x){
+    return lower_bound(v.begin(), v.end(), x)-v.begin()+1;
+}
+
+void ini(int x){
+    for(int i=1;i<=n;i++){
+        v.push_back(a[i]);
+    }
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()),v.end());
+    
+}

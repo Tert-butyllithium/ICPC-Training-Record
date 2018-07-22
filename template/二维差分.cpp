c05671e4577109624c594195a40216269abda484
Tert-butyllithium <40877238+Tert-butyllithium@users.noreply.github.com>
2018-07-22 15:19:04 +0800

    Create 二维差分.cpp

---
+++ template/二维差分.cpp
@@ -0,0 +1,13 @@
+//构建
+while(q--){
+    scanf("%d%d%d%d%d",&x1,&y1,&x2,&y2,&val);
+    cnt[x1][y1]++;
+		cnt[x1][y2+1]--;
+		cnt[x2+1][y1]--;
+		cnt[x2+1][y2+1]++;
+}
+
+//恢复
+for(int i=1;i<=n;i++)
+   for(int j=1;j<=m;j++)
+        cnt[i][j]+=cnt[i-1][j]+cnt[i][j-1]-cnt[i-1][j-1];

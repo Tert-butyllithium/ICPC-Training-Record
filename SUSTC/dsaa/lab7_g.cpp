7221c8f31c8111451a9229c72c60a2a755ad1482
Tert-butyllithium <40877238+Tert-butyllithium@users.noreply.github.com>
2018-11-18 15:41:23 +0800

    Update lab7_g.cpp

--- SUSTC/dsaa/lab7_g.cpp
+++ SUSTC/dsaa/lab7_g.cpp
@@ -8,18 +8,18 @@ typedef tree<int, null_type, greater<int>, rb_tree_tag, tree_order_statistics_no
 const int maxn=40005;
 bool vis[maxn];
 int arr[maxn];
-int chart[maxn][200];
+int chart[maxn][202];
 pair<int,int> pairs[maxn];
 int n, m;
 void get_chart(int x, int y)
 {
     rbtree pq;
-    for (int i = x; i < n; i += y)
+    for (int i = x; i <= n; i += y)
     {
         pq.insert(arr[i]);
     }
     chart[x][y] = *pq.find_by_order(m);
-    for (int i = x+y; i < n; i += y)
+    for (int i = x+y; i <= n; i += y)
     {
         pq.erase(pq.find(arr[i - y]));
         if(pq.size()>=m)
@@ -41,15 +41,25 @@ int main(int argc, char const *argv[]){
         for(int i=0;i<n;i++){
             int x,y;
             scanf("%d%d",&x,&y);
-            vis[y]=true;
+            pairs[i] = make_pair(x, y);
+            vis[y] = true;
         }
         m = (int)sqrt(n);
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i <= m+1; i++)
         {
             if(!vis[i]) continue;
+            for (int x = 1; x <= i;x++){
+                get_chart(x, i);
+            }
         }
-
-
+        for (int i = 0; i < n;i++){
+            if(pairs[i].second>m+1){
+                puts("-1");
+            }
+            else{
+                printf("%d\n", chart[pairs[i].first][pairs[i].second]);
+            }
+        }
     }
     return 0;
 }

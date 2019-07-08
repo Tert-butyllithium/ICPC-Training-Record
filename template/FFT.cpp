99768eeb4644aff06649215e469018696870ffdf
Tert-butyllithium <40877238+Tert-butyllithium@users.noreply.github.com>
2019-07-08 21:23:02 +0800

    Update FFT.cpp

--- template/FFT.cpp
+++ template/FFT.cpp
@@ -1,39 +1,60 @@
-//仅仅展示代码片段，目的求多项式的平方
-
-void FFT(complex<double> x[], int n, int p) {
-    for (int i=0,t=0;i<n;i++)
+inline void FFT(vector<CD> &a, bool inverse)
+{
+    int n = a.size();
+    // 原地快速 bit reversal
+    for (int i = 0, j = 0; i < n; i++)
     {
-        if (i>t) swap(x[i],x[t]);
-        for (int j=n>>1;(t^=j)<j;j>>=1);
+        if (j > i)
+            swap(a[i], a[j]);
+        int k = n;
+        while (j & (k >>= 1))
+            j &= ~k;
+        j |= k;
     }
-    for (int m=2;m<=n;m<<=1)  //枚举每一层
+    double pi = inverse ? -PI : PI;
+    for (int step = 1; step < n; step <<= 1)
     {
-        complex<double>       wn(cos(p*2*pi/m),sin(p*2*pi/m));
-        for (int i=0;i<n;i+=m)
+        // 把每相邻两个“step 点 DFT”通过一系列蝴蝶操作合并为一个“2*step 点 DFT”
+        double alpha = pi / step;
+        // 为求高效，我们并不是依次执行各个完整的 DFT 合并，而是枚举下标 k
+        // 对于一个下标 k，执行所有 DFT 合并中该下标对应的蝴蝶操作，即通过 E[k] 和 O[k] 计算 X[k] 
+        // 蝴蝶操作参考:http://en.wikipedia.org/wiki/Butterfly_diagram
+        for (int k = 0; k < step; k++)
         {
-            complex<double> w(1,0),u;
-            int k=m>>1;
-            for (int j=0;j<k;j++,w*=wn)
-            {
-                //蝴蝶操作
-                u=x[i+j+k]*w;
-                x[i+j+k]=x[i+j]-u;
-                x[i+j]=x[i+j]+u;
+            // 计算 omega^k. 这个方法效率低，但如果用每次乘 omega 的方法递推会有精度问题。
+            // 有更快更精确的递推方法，为了清晰起见这里略去
+            CD omegak = exp(CD(0, alpha * k));
+            for (int Ek = k; Ek < n; Ek += step << 1)
+            {                       //Ek 是某次 DFT 合并中 E[k] 在原始序列中的下标
+                int Ok = Ek + step; // Ok 是该 DFT 合并中 O[k] 在原始序列中的下标 
+                CD t = omegak * a[Ok]; // 蝴蝶操作:x1 * omega^k 
+                a[Ok]=a[Ek]-t; // 蝴蝶操作:y1=x0-t
+                a[Ek] += t;         // 蝴蝶操作:y0=x0+t
             }
         }
     }
+    if (inverse)
+        for (int i = 0; i < n; i++)
+            a[i] /= n;
+}
+// 用 FFT 实现的快速多项式乘法
+inline vector<double> operator*(const vector<double> &v1, const vector<double> &v2)
+{
+    int s1 = v1.size(), s2 = v2.size(), S = 2;
+    while (S < s1 + s2)
+        S <<= 1;
+    vector<CD> a(S, 0), b(S, 0); // 把 FFT 的输入长度补成 2 的幂，不小于 v1 和 v2 的长度之和 
+    for(int i = 0; i < s1; i++) a[i] = v1[i];
+    FFT(a, false);
+    for (int i = 0; i < s2; i++)
+        b[i] = v2[i];
+    FFT(b, false);
+    for (int i = 0; i < S; i++)
+        a[i] *= b[i];
+    FFT(a, true);
+    vector<double> res(s1 + s2 - 1);
+    for (int i = 0; i < s1 + s2 - 1; i++)
+        res[i] = a[i].real(); // 虚部均为 0
+    return res;
 }
 
-//扩展成最近的2^n
-int NTT_init(int n_) {
-    int n = 1;
-    for (; n < n_; n <<= 1);
-    return n;
-}
-
-// main 里面的代码片段
-    FFT(ra, len, 1);
-    for (int i = 0; i < len; i++) {
-        ra[i] = ra[i] * ra[i];
-    }
-    FFT(ra, len, -1);
